7208076622e60d275c84bc8f38a871a475ce6eb4
Euglis Lopez <lopez.ramon1988@gmail.com>
2022-08-05 10:16:44 -0400

    Ajustes de cors

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout','forget-password', 'register', 'reset-password', 'resend-email', 'email/*'],
 
     'allowed_methods' => ['*'],
 
