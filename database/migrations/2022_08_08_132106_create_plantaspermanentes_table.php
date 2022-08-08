d3ffea3707d04db82575d3a1d8af49d0d7c9570e
Veronica <veronicam@epidataconsulting.com>
2022-08-08 14:02:10 -0300

    creacion tabla permanente

---
+++ database/migrations/2022_08_08_132106_create_plantaspermanentes_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plantaspermanentes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('agente_id')->constrained('agentes');
+            
+            // esto ya esta en la tabla agente, lo de nivel y grado, va de nuevo?
+            $table->string('letra_nivel');
+            $table->string('numero_grado');
+            
+             
+            $table->string('tramo');
+            $table->string('agrupamiento');
+
+            $table->string('modalidad_vinculacion');
+            $table->string('estado_agente');
+            $table->string('funcion');
+            $table->date('ejercicio');
+            $table->string('numero_expediente');
+            $table->string('estado_expediente');
+            $table->string('numero_formulario');
+            $table->string('nivel_formulario');
+            $table->string('calificacion');
+            $table->integer('puntaje');
+            $table->string('evaluador');
+
+            //relaciones
+            $table->integer('area_id');
+
+            $table->string('unidad_analisis');
+            $table->boolean('notificacion');
+            $table->string('numero_notificacion')->nullable();
+            $table->string('observacion');
+            $table->boolean('corrimiento_grado'); 
+            $table->string('numero_expediente_grado');
+            $table->boolean('corrimiento_agrupamiento');
+            $table->string('numero_expediente_agrupacion');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plantaspermanentes');
+    }
+};
