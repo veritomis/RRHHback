a74dc1fad3d6016ccefb3ea1b14ca0b1deffa663
Veronica Miszel <veronicam@epidataconsulting.com>
2022-08-17 11:11:06 -0300

    Update PuestoFamiliaFactory.php

--- database/factories/PuestoFamiliaFactory.php
+++ database/factories/PuestoFamiliaFactory.php
@@ -30,7 +30,7 @@ class PuestoFamiliaFactory extends Factory
         }
 
         return [
-            'nombre' => $this->faker->text($this->faker->numberBetween(5, 255)),
+            'nombre' => $this->faker->text($this->faker->numberBetween(5, 20)),
             'puesto_grupo_id' => $puestosGrupo->id,
             'created_at' => $this->faker->date('Y-m-d H:i:s'),
             'updated_at' => $this->faker->date('Y-m-d H:i:s')
