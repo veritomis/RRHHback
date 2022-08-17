848ad160d73ff7b9f62ee5edf9320c08f7e26404
Veronica Miszel <veronicam@epidataconsulting.com>
2022-08-17 11:11:30 -0300

    Update PuestoGrupoFactory.php

--- database/factories/PuestoGrupoFactory.php
+++ database/factories/PuestoGrupoFactory.php
@@ -24,7 +24,7 @@ class PuestoGrupoFactory extends Factory
     {
         
         return [
-            'nombre' => $this->faker->text($this->faker->numberBetween(5, 255)),
+            'nombre' => $this->faker->text($this->faker->numberBetween(5, 20)),
             'created_at' => $this->faker->date('Y-m-d H:i:s'),
             'updated_at' => $this->faker->date('Y-m-d H:i:s')
         ];
